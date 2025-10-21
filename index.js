9f5b498d3ff9dcc3230e28097c83661d60da605c
TRANZCEND3NCE <tranzcend3nce@gmail.com>
2025-10-20 23:37:34 -0400

    second commit

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ let choices = ["rock", "paper", "scissors"];
 let humanScore = 0;
 let computerScore = 0;
 
-const getHumanChoice = () => {
+const getComputerChoice = () => {
   let randomChoice = Math.floor(Math.random() * choices.length);
+  return choices[randomChoice];
+};
+
+const getHumanChoice = () => {
+  let choice = prompt("Please choose rock, paper or scissors").toLowerCase().trim();
+
+  if (choices.includes(choice)) {
+    return choice;
+  } else {
+    alert("Invalid choice. Please try again.");
+    return getHumanChoice();
+  }
+};
+
+const playRound = () => {
+  
 }
