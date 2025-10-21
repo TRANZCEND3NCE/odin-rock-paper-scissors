6041e4749e9380d0473f8ab52fd13f86c433bbc7
TRANZCEND3NCE <tranzcend3nce@gmail.com>
2025-10-20 22:02:20 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let choices = ["rock", "paper", "scissors"];
+
+let humanScore = 0;
+let computerScore = 0;
+
+const getHumanChoice = () => {
+  let randomChoice = Math.floor(Math.random() * choices.length);
+}
